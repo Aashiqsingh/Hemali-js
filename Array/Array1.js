c5df43fdd038b7d35a94400fc198d9a269c6091a
Aashiqsingh <aashiqsingh56789@gmail.com>
2025-01-06 15:12:29 +0530

    array

---
+++ Array/Array1.js
@@ -0,0 +1,45 @@
+// Array : Array is a collection of data.
+
+// array  : hetrogenus  
+
+
+// arr = [12,34,5.66,8.99,"ram","seeta",true , false]
+
+// // console.log(arr);
+// console.log(arr[0]);
+// console.log(arr[5]);
+
+
+
+
+// console.log(user.length);
+
+// for(let i=0;i<user.length;i++){
+
+//     console.log(user[i].toUpperCase());
+    
+// }
+
+// for(let i=0;i<user.length;i++){
+
+//     if(user[i].length > 5)
+//     {
+//         console.log(user[i]);
+        
+//     }
+    
+// }
+
+var user = ["hemali","dev","nency","rahul","bhumi","sonali"]
+console.log(user);
+
+for(let i=0;i<user.length;i++){
+
+    if(user[i].includes("i"))
+        {
+            console.log(user[i]);
+        }    
+    
+}
+
+
---
+++ Array/Array2.js
@@ -0,0 +1,91 @@
+var arr = [12,13,14,15]
+
+// var x = user.concat(arr)
+// console.log(x);
+
+// var x = user.includes("nency")
+// console.log(x);
+
+// var x = user.indexOf("nency")
+// console.log(x);
+
+// var x = user.join(" ")
+// console.log(x);
+
+// var x = user.reverse()
+// console.log(x);
+
+// var x = user.slice(1,5)
+// console.log(x);
+
+// var x = user.slice(-3,-1)
+// console.log(x);
+
+// var x = user.sort()
+// console.log(x);
+var user = ["hemali","dev","nency","rahul","bhumi","sonali"]
+
+
+// user.splice(3,0,"thakkar")
+// // user.splice(3,2,"thakkar")
+// user.splice(3,2,"thakkar","patel","shah","singh")
+
+
+
+// console.log(user);
+
+
+
+
+// var str = "hemali thakker"
+
+// var x = str.split("")
+// console.log(x);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// user.push("thakkar")
+// var x = user.push("thakkar","patel","ujjar")
+// console.log(x);
+
+
+// console.log(user);
+
+// user.pop()
+// user.pop()
+
+
+// console.log(user);
+
+// user.unshift("thakkar")
+// user.unshift("thakkar","patel","shah")
+
+
+// var x = user.shift()
+
+// console.log(x);
+
+
+// console.log(user);
+
+
+// var str = "hemali"
+
+// "ilameh"
